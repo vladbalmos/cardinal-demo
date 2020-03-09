986185f6b50d9ba2a367971d4a5567ad7abfb7ae
Vlad Balmos <vlad.balmos@code932.com>
2020-03-09 16:16:50 +0200

    Upgrade cardinal

---
+++ copy-cardinal-from-pskxyz.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r ../privatesky-xyz/cardinal .
+cp -r ../privatesky-xyz/cardinal.js cardinal.js
+cp -r ../privatesky-xyz/themes .
+cp -r ../privatesky-xyz/scripts/browserSupport.js scripts/browserSupport.js
+cp -r ../privatesky-xyz/scripts/privatesky scripts/
+cp -r ../privatesky-xyz/scripts/controllers/base-controllers scripts/controllers
